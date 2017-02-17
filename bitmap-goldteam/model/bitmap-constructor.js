456f2e25abccd6fc1541ebb5f4cfca194f6b2599
noahgribbin <noahgribbin@yahoo.com>
2017-02-17 14:17:36 -0800

    actually made Async this time

--- bitmap-goldteam/model/bitmap-constructor.js
+++ bitmap-goldteam/model/bitmap-constructor.js
@@ -4,39 +4,25 @@ const fs = require('fs');
 const EE = require('events');
 const ee = new EE;
 
+var bitmap = {}
 
-const aaa = [];
-
-ee.on('abc', function(data) {
-  if (aaa.length !== 0) return;
+function readContent(callback) {
   fs.readFile(`${__dirname}/../img/palette-bitmap.bmp`, function(err, data) {
-    if(err) throw err;
-    console.log(data.toString('hex'));
-    aaa.push(data.toString('hex'));
-    console.log(aaa);
-    ee.emit('abc');
-    // console.log('constructed object:', bmp);
+    if(err) return  callback(err);
+    callback(null, data)
   });
-    // const bmp = new Image(bitmap);
-    // console.log('constructed object:', bmp);
-});
-ee.emit('abc');
+}
 
-// const bitmap = fs.readFile(`${__dirname}/../img/palette-bitmap.bmp`, function(err, data) {
-//   if(err) throw err;
-// });
-// console.log(bitmap);
-console.log(aaa);
-var Image = function(type, size, width, height, imageStart, pixelArray) {
-  this.type = bitmap.toString('utf-8', 0, 2);
-  this.size = bitmap.readInt32LE(2);
-  this.width = bitmap.readInt32LE(18);
-  this.height = bitmap.readInt32LE(22);
-  this.imageStart = bitmap.readInt32LE(10);
-  this.pixelArray = bitmap.toString('hex', this.imageStart, this.size);
-};
-//
-// console.dir(bmp);
-//
-// console.log(typeof(Image.pixelArray));
-// const bmp = new Image(bitmap);
+readContent(function (err, data) {
+  const bitmap = data;
+  var Image = function(type, size, width, height, imageStart, pixelArray) {
+    this.type = bitmap.toString('utf-8', 0, 2);
+    this.size = bitmap.readInt32LE(2);
+    this.width = bitmap.readInt32LE(18);
+    this.height = bitmap.readInt32LE(22);
+    this.imageStart = bitmap.readInt32LE(10);
+    this.pixelArray = bitmap.toString('hex', this.imageStart, this.size);
+  };
+  const bpm = new Image(bitmap);
+  console.log('in:', bpm);
+});
