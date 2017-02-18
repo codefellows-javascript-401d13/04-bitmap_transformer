348bc1e2932f1f50e4a4fce201d8f82f1ee8c6ba
noahgribbin <noahgribbin@yahoo.com>
2017-02-17 18:03:06 -0800

    move constructor from function call to function

--- bitmap-goldteam/model/bitmap-constructor.js
+++ bitmap-goldteam/model/bitmap-constructor.js
@@ -4,23 +4,21 @@ const fs = require('fs');
 
 module.exports = exports = {};
 
-function readContent(callback) {
+exports.readContent = function(callback) {
   fs.readFile(`${__dirname}/../img/palette-bitmap.bmp`, function(err, data) {
     if(err) return  callback(err);
-    callback(null, data)
+    callback(null, data);
+    const bitmap = data;
+    var Image = function(type, size, width, height, imageStart, pixelArray) {
+      this.type = bitmap.toString('utf-8', 0, 2);
+      this.size = bitmap.readInt32LE(2);
+      this.width = bitmap.readInt32LE(18);
+      this.height = bitmap.readInt32LE(22);
+      this.imageStart = bitmap.readInt32LE(10);
+      this.pixelArray = bitmap.toString('hex', this.imageStart, this.size);
+    };
+    var test = new Image;
+    console.log('var test:', test);
+    return test;
   });
-}
-
-readContent(function (err, data) {
-  const bitmap = data;
-  var Image = function(type, size, width, height, imageStart, pixelArray) {
-    this.type = bitmap.toString('utf-8', 0, 2);
-    this.size = bitmap.readInt32LE(2);
-    this.width = bitmap.readInt32LE(18);
-    this.height = bitmap.readInt32LE(22);
-    this.imageStart = bitmap.readInt32LE(10);
-    this.pixelArray = bitmap.toString('hex', this.imageStart, this.size);
-  };
-  const bpm = new Image(bitmap);
-  console.log('in:', bpm);
-});
+};
---
+++ bitmap-goldteam/model/color-constructor.js
@@ -0,0 +1,5 @@
+const g = require('./bitmap-constructor.js');
+
+g.readContent(function (err, data) {
+    
+});
