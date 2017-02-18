f0a80774a75a8212fce487da2c2aea573a31c96e
caylazabel <caylazabel@gmail.com>
2017-02-17 16:57:37 -0800

    pushing some tests up

---
+++ test/bitmap-constructor-test.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const expect = require('chai').expect;
+const bitmapConstructor = require('../model/bitmap-constructor.js');
+
+describe('Bitmap Constructor Module', function(){
+  describe('when passed buffer object', function(){
+    it('should instantiate a bitmap', function(){
+
+    });
+    it('should have the correct properties', function(){
+
+    };
+
+    })
+  });
+};
+
+describe
