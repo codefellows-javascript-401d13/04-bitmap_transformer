3569dc58a8664f9dfca2cf60d415bac70621fbdb
Anna Ulanova <anetteua0@gmail.com>
2017-02-17 21:00:14 -0800

    inverted bitmap writes to file

--- bitmap-annashivwill/index.js
+++ bitmap-annashivwill/index.js
@@ -4,9 +4,12 @@ const fs = require('fs');
 const transformations = require(`${__dirname}/lib/transformations.js`);
 
 fs.readFile(`${__dirname}/img/palette-bitmap.bmp`, function(err, data) {
-  //// ERROR
   if (err) throw err;
   const bitmap = data;
   transformations.invert(bitmap);
 
+  fs.writeFile(`${__dirname}/img/transformed.bmp`, data, function(err, data) {
+    if (err) throw err;
+    console.log('write file msg:', 'new file created');
+  });
 });
--- bitmap-annashivwill/lib/transformations.js
+++ bitmap-annashivwill/lib/transformations.js
@@ -5,13 +5,14 @@ const image = require(`${__dirname}/../index.js`);
 
 module.exports = exports = {};
 
-exports.turnBlack = function(tableString) {
-  return '0'.repeat(2048);
+exports.turnBlack = function(bitmap) {
+  return bitmap.fill('0', 54, 1078);
 };
 
 exports.invert = function(bitmap) {
-  for (var i = 0; i < bitmap.length; i ++) {
+  for (var i = 54; i < 1078; i ++) {
     var invBitmap = bitmap.fill(255-bitmap[i], i, i+1);
   };
+  console.log(invBitmap);
   return invBitmap;
 };
