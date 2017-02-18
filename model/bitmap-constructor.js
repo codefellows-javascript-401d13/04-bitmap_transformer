7f32da329ebce51aa3a19ecf31276d8dc50d2ba1
Remil Marzan <remilonwheels@gmail.com>
2017-02-18 04:40:02 -0800

    transform functions done

--- model/bitmap-constructor.js
+++ model/bitmap-constructor.js
@@ -1,11 +1,16 @@
 'use strict';
 
-const Bitmap = function Bitmap(buffer){
+const Bitmap = module.exports = function Bitmap(buffer){
+  this.size = buffer.readInt32LE(2);
+  this.imageStart = buffer.readInt32LE(10);
+  this.headerSize = buffer.readInt32LE(14);
+  this.imageSize = buffer.readInt32LE(34);
+  this.numColors = buffer.readInt32LE(46);
   this.head = buffer.toString('hex', 0, 54);
   let bitmapColorArray = [];
-  let colorMapStart = 54;
+  let colorMapStart = 14 + this.headerSize;
   let singleColorStart = colorMapStart;
-  for(let i = 0; i < 256; i++){
+  for(let i = 0; i < this.numColors; i++){
     let singleColorArray = [];
     for(let j = singleColorStart; j < (singleColorStart + 4); j++){
       singleColorArray.push(buffer.readUInt8(j));
@@ -14,43 +19,23 @@ const Bitmap = function Bitmap(buffer){
     singleColorStart += 4;
   }
   this.color = bitmapColorArray;
-  this.pixel = buffer.toString('hex', 1078, 11078);
+  this.pixel = buffer.toString('hex', this.imageStart, this.imageStart + this.imageSize);
 };
 
 Bitmap.colorMapToString = function(array) {
-  // var color = '';
-  // for(let i = 0; i < array.length; i++){
-  //   for(let j = 0; j < array[i].length; j++){
-  //     let temp = array[i][j].toString(16);
-  //     if(temp === '0') temp = '00';
-  //     color += temp;
-  //   }
-  // }
-  // return color;
-
-  let buffer = new Buffer.alloc(256);
-
-  array.forEach(color => {
-    color.forEach(channel => {
-      w
-    });
-  });
-
-  function channelToBuffer(channel) {
-    buffer.writeInt8()
+  var color = '';
+  for(let i = 0; i < array.length; i++){
+    for(let j = 0; j < array[i].length; j++){
+      let temp = array[i][j].toString(16);
+      if (array[i][j] < 16) temp = `0${temp}`;
+      color += temp;
+    }
   }
-
-
+  return color;
 };
 
 Bitmap.prototype.transformPixel = function transformPixel(transformFn) {
   return this.color.map(transformFn);
-};
-
-
-Bitmap.zeroFirstTransform = function zeroFirstTransform(color) {
-  // return [0, color[1], color[2], color[3]];
-  return [0, color[1], color[2], color[3]];
 };
 
 Bitmap.invertTransform = function invertTransform(color) {
@@ -60,9 +45,15 @@ Bitmap.invertTransform = function invertTransform(color) {
   return [invert(color[0]), invert(color[1]), invert(color[2]), color[3]];
 };
 
-module.exports = Bitmap;
-
-
-// var manipData = colorMapToString(transformPixel(bitmapColorArray, zeroFirstTransform));
-//
-// var invertData = colorMapToString(transformPixel(bitmapColorArray, invertTransform));
+Bitmap.blueTransform = function blueTransform(color) {
+  function blue(color, level) {
+    return Math.min(255, Math.floor(color * level));
+  }
+  return [255, blue(color[1], .5), blue(color[2], .5), color[3]];
+};
+Bitmap.grayTransform = function grayTransform(color) {
+  function gray(color, level) {
+    return Math.min(255, Math.floor(color * level));
+  }
+  return [gray(color[0], .15), gray(color[1], .1), gray(color[2], .12), color[3]];
+};
