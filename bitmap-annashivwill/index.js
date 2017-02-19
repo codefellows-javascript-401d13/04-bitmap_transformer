1d5c77664dca35f0a9d484c597b3565dc0f4110d
Anna Ulanova <anetteua0@gmail.com>
2017-02-19 13:56:12 -0800

    reset

--- bitmap-annashivwill/index.js
+++ bitmap-annashivwill/index.js
@@ -1,26 +1,5 @@
 'use strict';
 
-const fs = require('fs');
-const transformations = require(`${__dirname}/lib/transformations.js`);
+const maps = require(`${__dirname}/model/read-write.js`);
 
-fs.readFile(`${__dirname}/img/palette-bitmap.bmp`, function(err, data) {
-  if (err) throw err;
-  const bitmap = data;
-
-  fs.writeFile(`${__dirname}/img/inverted.bmp`, transformations.invert(bitmap), function(err, data) {
-    if (err) throw err;
-  });
-
-  fs.writeFile(`${__dirname}/img/black.bmp`, transformations.turnBlack(bitmap), function(err, data) {
-    if (err) throw err;
-  });
-
-  fs.writeFile(`${__dirname}/img/red.bmp`, transformations.redScale(bitmap), function(err, data) {
-    if (err) throw err;
-  });
-
-  fs.writeFile(`${__dirname}/img/grey.bmp`, transformations.greyScale(bitmap), function(err, data) {
-    if (err) throw err;
-  });
-
-});
+maps.readWrite();
