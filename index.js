617b91f36a67366a62975efc84555567e9f54910
Zachary Crumbo <zcrumbo@mac.com>
2017-02-19 10:39:08 -0800

    deleted practice files, edited to pass linter, cleaned up console.logs and added error if index is called without transform parameters

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ const len = process.argv.length;
 
 if (len>2) {
   for (let i = 2; i < len; i++) {
-    let arg = process.argv[i]
-    console.log(arg);
+    let arg = process.argv[i];
     transform[arg]('pallette-bitmap.bmp', `${arg}-bitmap.bmp`);
   }
-};
+  return
+}
+throw new Error('You must supply a transform command: grayscale, invert, bluescale or random)');
--- model/colorconst.js
+++ model/colorconst.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 const readWrite = require('../lib/bitmap-helper');
 
@@ -8,7 +8,7 @@ exports.invert = function(inputFile, outputFile){
   readWrite.read(inputFile, function(err, parsedInfo, bitmap){
     for (var key of parsedInfo.colorTable.keys()) {
       if((key+1)%4!==0) parsedInfo.colorTable[key] = 255 - parsedInfo.colorTable[key];
-    };
+    }
     readWrite.write(null, bitmap, outputFile);
   });
 };
@@ -18,25 +18,26 @@ exports.grayscale = function(inputFile, outputFile){
     //let pixelObj = {};
     for (var key of parsedInfo.colorTable.keys()) {
       if ((key+1)%4!==0){
-      if (highestValue < parsedInfo.colorTable[key]) highestValue = parsedInfo.colorTable[key];
-      };
+        if (highestValue < parsedInfo.colorTable[key]) highestValue = parsedInfo.colorTable[key];
+      }
       if ((key+1)%4==0) {
         parsedInfo.colorTable[key-1] = highestValue;
         parsedInfo.colorTable[key-2] = highestValue;
         parsedInfo.colorTable[key-3] = highestValue;
         highestValue = 0;
-        }
-    readWrite.write(null, bitmap, outputFile);
-    };
+      }
+      readWrite.write(null, bitmap, outputFile);
+    }
   });
 };
+
 exports.random = function(inputFile, outputFile){
   readWrite.read(inputFile, function(err, parsedInfo, bitmap){
     var topNum = (Math.random() * 2) + 1;
     for (var key of parsedInfo.colorTable.keys())  {
       if((key+1)%4!==0) parsedInfo.colorTable[key] = topNum * parsedInfo.colorTable[key];
-    readWrite.write(null, bitmap, outputFile);
-    };
+      readWrite.write(null, bitmap, outputFile);
+    }
   });
 };
 
@@ -45,13 +46,14 @@ exports.bluescale = function(inputFile, outputFile){
     let blueValue = 0;
     for (var key of parsedInfo.colorTable.keys()) {
       if ((key+1)%4!==0){
-      if (key%4==0) blueValue = parsedInfo.colorTable[key];
-      };
+        if (key%4==0) blueValue = parsedInfo.colorTable[key];
+      }
       if ((key+1)%4==0) {
         parsedInfo.colorTable[key-1] = 30;
         parsedInfo.colorTable[key-2] = 30;
-        }
-    readWrite.write(null, bitmap, outputFile);
-    };
+        parsedInfo.colorTable[key-3] = blueValue;
+      }
+      readWrite.write(null, bitmap, outputFile);
+    }
   });
 };
