c11e9b4c24dd9be087f9eacd1ff433eb2d48ec33
Jeremiah <jeremiaht.walters@gmail.com>
2017-02-18 17:35:38 -0800

    four test on bitmap constructor

--- test/bithelp-test.js
+++ test/bithelp-test.js
@@ -3,22 +3,54 @@
 const expect = require('chai').expect;
 const bitmapFileHelper = require ('../lib/bitmap-helper.js');
 
-describe('fileReader', function(){
- describe(' file is being read', function(){
-   it('should return file has been read', function(done){
-    bitmapFileHelper.read(`${__dirname}/../img/palette-bitmap.bmp`, function(err, data){
-    expect(data.numOfColors).to.equal(256);
-    done();
+describe('testing color-transform.js', function(){
+  describe(' Bitmap constructor test', function(){
+    it('should return number of colors', function(done){
+      bitmapFileHelper.read(`${__dirname}/../img/palette-bitmap.bmp`, function(err, data){
+        expect(data.numOfColors).to.equal(256);
+        done();
+      });
     });
-   });
 
+  });
 });
+describe('testing color-transform.js', function(){
+  describe(' Bitmap constructor test', function(){
+    it('should return pixel array offset', function(done){
+      bitmapFileHelper.read(`${__dirname}/../img/palette-bitmap.bmp`, function(err, data){
+        expect(data.pixelArrayOffset).to.equal(1078);
+        done();
+      });
+    });
 
+  });
+});
+describe('testing color-transform.js', function(){
+  describe(' Bitmap constructor test', function(){
+    it('should size', function(done){
+      bitmapFileHelper.read(`${__dirname}/../img/palette-bitmap.bmp`, function(err, data){
+        expect(data.size).to.equal(11078);
+        done();
+      });
+    });
+
+  });
+});
+describe('testing color-transform.js', function(){
+  describe(' Bitmap constructor test', function(){
+    it('should return the correct type', function(done){
+      bitmapFileHelper.read(`${__dirname}/../img/palette-bitmap.bmp`, function(err, data){
+        expect(data.type).to.equal('BM');
+        done();
+      });
+    });
+
+  });
 });
 // describe('FS Module', function(){
 //   describe('with an improper file path', function(){
 //     it('should return file path error', function(done){
-//       fileReader('fake.txt', 'file.txt', 'paths.txt', function(err){
+//       testing color-transform.js('fake.txt', 'file.txt', 'paths.txt', function(err){
 //         expect(err).to.be.an('error');
 //         done();
 //       });
@@ -26,7 +58,7 @@ describe('fileReader', function(){
 //   });
 //   describe('with an out of order data return', function(){
 //     it('should flag it as out of order', function(done){
-//       fileReader('one.txt', 'two.txt', 'three.txt', dataReturn);
+//       testing color-transform.js('one.txt', 'two.txt', 'three.txt', dataReturn);
 //       function dataReturn(err ,data){
 //         var testData = new Array('31207468', '3220636f', '74686973');
 //         expect(data[0]).to.equal(testData[0]);
@@ -37,5 +69,3 @@ describe('fileReader', function(){
 //     });
 //   });
 // });
-
-
