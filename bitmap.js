78cef9f7616ab9b899da8b28e8283d3e9a72f238
Remil Marzan <remilonwheels@gmail.com>
2017-02-19 16:37:34 -0800

    now takes multiple files and added optimus-prime bmp to convert

--- bitmap.js
+++ bitmap.js
@@ -1,40 +1,36 @@
-'use strict';
+'use strict'; 
 
 const fileHelper = require('./lib/bitmap-file-helper.js');
 const Bitmap = require('./model/bitmap-constructor.js');
 
 
-let filesToConvert = [`${__dirname}/img/palette-bitmap.bmp`];
+let filesToConvert = [`${__dirname}/img/palette-bitmap.bmp`, `${__dirname}/img/optimus-prime.bmp`];
 transformBMP(filesToConvert);
 
 function transformBMP(files) {
+  let colorTransforms = [];
   if (process.argv.length > 2) {
     for (let i = 2; i < process.argv.length; i++) {
       if (Bitmap.colorTransforms.includes(process.argv[i])) {
-        fileHelper(files[0], getShortFilename(files[0]), process.argv[i]);
+        colorTransforms.push(process.argv[i]);
       }
     }
-    return;
+    return fileColorTransform(files, colorTransforms);
+
   }
-  fileHelper(`${__dirname}/img/palette-bitmap.bmp`, 'palette', 'invertTransform');
-  fileHelper(`${__dirname}/img/palette-bitmap.bmp`, 'palette', 'grayTransform');
-  fileHelper(`${__dirname}/img/palette-bitmap.bmp`, 'palette', 'blueTransform');
+  colorTransforms = ['invertTransform', 'grayTransform', 'blueTransform'];
+
+  return fileColorTransform(files, colorTransforms);
+
+
+  function fileColorTransform(files, colorTransforms) {
+    files.forEach( file => {
+      colorTransforms.forEach(transform => {
+        fileHelper(file, getShortFilename(file), transform);
+      });
+    });
+  }
 }
-// function transformBMP() {
-//   // let filesToConvert = [];
-//   if (process.argv.length > 2) {
-//     for (let i = 2; i < process.argv.length; i++) {
-//       if (Bitmap.colorTransforms.includes(process.argv[i])) {
-//
-//         fileHelper(`${__dirname}/img/palette-bitmap.bmp`, 'palette', process.argv[i]);
-//       }
-//     }
-//     return;
-//   }
-//   fileHelper(`${__dirname}/img/palette-bitmap.bmp`, 'palette', 'invertTransform');
-//   fileHelper(`${__dirname}/img/palette-bitmap.bmp`, 'palette', 'grayTransform');
-//   fileHelper(`${__dirname}/img/palette-bitmap.bmp`, 'palette', 'blueTransform');
-// }
 
 function getShortFilename(fullPath) {
   let filename = fullPath.slice(fullPath.lastIndexOf('/') + 1, fullPath.indexOf('.bmp'));
