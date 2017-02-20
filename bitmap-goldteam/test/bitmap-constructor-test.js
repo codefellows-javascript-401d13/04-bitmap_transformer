7d2a350d8a1db67fa85cf0e2baab4ecbe155a43b
Gary Lundgren <builtbygl@gmail.com>
2017-02-19 20:27:13 -0800

    basic test template

---
+++ bitmap-goldteam/test/bitmap-constructor-test.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const expect = require('chai').expect;
+const constructor = require('../lib/bitmap-constructor.js');
+
+describe('File Reading Module', function(err, data) {
+  describe('it should err for a empty buffer', function(err, data) {
+    it('should return an error for empty buffer', function(){
+       // this will check buffer is empty before load
+    });
+  });
+  describe('it should check that read buffer is the same as our buffer object', function(err, data){
+    it('should be the same as our object', function() {
+      // read the buffer and compare to our object
+    })
+  })
+});
